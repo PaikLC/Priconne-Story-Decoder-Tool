92590bd037c70555456dd47f1f26db579e1131cf
MRI <ofranciscoisaac@yahoo.com>
2023-10-01 06:58:56 -0600

    Adding more options to process the data and how to save it. Still not working... but soon will be.

--- main.c
+++ main.c
@@ -15,6 +15,8 @@ const char *help =
     "Arguments:\n"
         "\t-p                   The path to the file to deserialize\n"
         "\t-o                   The path to the directory to save de result\n"
+        "\t-b                   Save the data in a binary format.\n"
+        "\t-t                   Save the data in a text file (Default)\n"
         "\t\n" //More simple commands??
     "Long arguments\n"
         "\t--all-data           Put in a txt file all the details of the file\n"
@@ -24,8 +26,8 @@ const char *help =
         "\t                     SYS: for System category\n"
         "\t                     MOT: for Motion category\n"
         "\t                     EFC: for Effect category\n\n"
-        "\t--only-cat-number N  Just save the specified category number\n"
-        "\t                     They are 112 but the starts form -1 to 110\n"
+        "\t--only-cat-number N  Just save the specified category number (Not working...)\n"
+        "\t                     They are 112 but it starts counting form -1 to 110\n"
         "\t                     For more details see the source code or docs\n" // Docs if I have the time... and motivation
 ;
 
@@ -86,7 +88,8 @@ void saveResult(char *path, ARGS *story)
 int main(int argc, char *argv[])
 {
     char *pathOpen = NULL, *pathSave = NULL;
-    bool tmp = false;
+    bool tmp = false, binary = false;
+    int catFilter = -1; // -1 for all
 
     ARGS story = { 0 };
 
@@ -108,16 +111,70 @@ int main(int argc, char *argv[])
             pathSave = argv[i + 1];
             break;
 
+        case 'b':
+            binary = true;
+            break;
+
+        case 't':
+            binary = false;
+            break;
+
         case '-':
             {
                 if (strcmp(argv[i], "--all-data") == 0)
                 {
                     tmp = true;
                 }
+                else if (strcmp(argv[i], "--only-category") == 0)
+                {
+                    if (i + 1 == argc)
+                    {
+                        fprintf(stderr, "Error: Missing argument!\n");
+                        return EXIT_FAILURE;
+                    }
+                    
+                    if (argv[1 + 1][0] == '-')
+                    {
+                        fprintf(stderr, "Error: Missing argument on \"--only-category\"\n");
+                        return EXIT_FAILURE;
+                    }
+                    else if (strcmp(argv[i + 1], "NON") == 0)
+                    {
+                        catFilter = 0;
+                    }
+                    else if (strcmp(argv[i + 1], "SYS") == 0)
+                    {
+                        catFilter = 1;
+                    }
+                    else if (strcmp(argv[i + 1], "MOT") == 0)
+                    {
+                        catFilter = 2;
+                    }
+                    else if (strcmp(argv[i + 1], "EFC") == 0)
+                    {
+                        catFilter = 3;
+                    }
+                    else
+                    {
+                        fprintf(stderr, "Error: Unknown category\n");
+                        return EXIT_FAILURE;
+                    }
+                }
+                else if(strcmp(argv[i], "--only-cat-number") == 0)
+                {
+                    if (i + 1 == argc)
+                    {
+                        fprintf(stderr, "Error: Missing argument!\n");
+                        return EXIT_FAILURE;
+                    }
+                    fprintf(stderr, "Warning: This command is still on develpment and doesn't work\n\n");
+                }
             }
             break;
 
         default:
+            fprintf(stderr, "Error: Unknown Command\n");
+            return EXIT_FAILURE;
             break;
         }
     }
@@ -128,6 +185,7 @@ int main(int argc, char *argv[])
         pathSave = (char *)malloc( sizeof(char) * strlen(pathOpen) + 1 );
         if (pathSave == NULL)
         {
+            fprintf(stderr, "Error: Can't create a save path\n");
             return EXIT_FAILURE;
         }
         
@@ -136,18 +194,32 @@ int main(int argc, char *argv[])
         if (pdest == NULL)
         {
             pathSave = (char *)realloc(pathSave, strlen(pathOpen) + 5);
-            strcat(pathSave, ".txt");
+            if (binary)
+            {
+                strcat(pathSave, ".bin");
+            }
+            else
+            {
+                strcat(pathSave, ".txt");
+            }
         }
         else
         {
-            strncpy(pdest, ".txt", 5);
+            if (binary)
+            {
+                strncpy(pdest, ".bin", 5);
+            }
+            else
+            {
+                strncpy(pdest, ".txt", 5);
+            }
         }
         
     }
 
     if(deserializeFile(pathOpen, &story) == 0)
     {
-        printf("Error Something happend...\n");
+        printf("Error: Something happend...\n");
         system("pause");
         freeArguments(&story);
         return EXIT_FAILURE;
